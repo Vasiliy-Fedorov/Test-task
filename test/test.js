b851e8f1f8de2c0dea7731859eccc522a3f24922
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-02-26 21:39:48 +0300

    trying to find selectors

--- test/test.js
+++ test/test.js
@@ -9,5 +9,22 @@ describe('Yandex', function () {
         assert.equal(ok, true, 'Page is not opened');
     });
 
+    it('search something', function(){
+        const text = $("#text");
+        const search_button = $(".button_theme_websearch");
+        const result = $(".serp-item:nth-child(5).organic__url-text");
+        const resultOk = result.isVisible();
+    //.serp-item:nth-child(5) .needsclick:nth-child(1)
+        text.setValue("Электронное облако");
+        search_button.click();
+        browser.pause(2000);
+      //  browser.waitForVisible(result);
+        result.click();
+        browser.pause(2000);
+
+
+        assert.equal(resultOk, true, 'Search is OK');
+    });
+
 
 });
