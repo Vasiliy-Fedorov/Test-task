9e426abf025b75e960e7303f4e04579377af87bc
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-03-17 19:52:52 +0300

    added screenShot funtional

--- test/test.js
+++ test/test.js
@@ -4,26 +4,26 @@ describe('Yandex', function () {
     it('page exist and opening', function(){
         browser.url('https://ya.ru');
         const start = $(".b-table__cell");
-        const ok = start.isVisible();
+        const startIsVisible = start.isVisible();
 
-        assert.equal(ok, true, 'Page is not opened');
+        assert.equal(startIsVisible, true, 'Page is not opened');
     });
 
     it('search something', function(){
         const text = $("#text");
         const search_button = $(".button_theme_websearch");
-        const result = $(".serp-item:nth-child(5).organic__url-text");
-        const resultOk = result.isVisible();
-    //.serp-item:nth-child(5) .needsclick:nth-child(1)
+
         text.setValue("Электронное облако");
         search_button.click();
-        browser.pause(2000);
-      //  browser.waitForVisible(result);
+        const result =$('[data-cid="2"]');
+        result.waitForVisible(2000);
         result.click();
-        browser.pause(2000);
+        browser.windowHandleFullscreen();
+
+        browser.saveScreenshot('./screenShots/exemple.png');
 
 
-        assert.equal(resultOk, true, 'Search is OK');
+        assert.equal(true, true, 'Search is OK');
     });
 
 
